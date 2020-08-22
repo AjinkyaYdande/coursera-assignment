1589a7ade91cf050a30680f63264d3367da8753e
Aznya <ajinkyavaydande57@gmail.com>
2020-08-22 23:24:57 +0530

    Added all file

---
+++ mod4solution/SpeakHello.js
@@ -0,0 +1,16 @@
+// function hell(){
+// 	console.log("Hello J");
+// }
+// hell();
+
+
+
+(function(window) {
+	var speakWord = "Hello";
+	var helloSpeaker = function (name) {
+		console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker = helloSpeaker;
+
+})(window);
---
+++ mod4solution/script.js
@@ -0,0 +1,25 @@
+// var names = ['Ajnkya','Jack','Joe','Jatt'];
+// for (var i = 0 ; i<names.length ; i++){
+// 	var firstLetter = names[i].charAt(0).toLowerCase();
+// }
+
+// print(firstLetter);
+
+
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
